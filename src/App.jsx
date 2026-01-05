4c219808a086c3d66859d0021efd6feac71cf8c3
TheshaAravinthan06 <aravinthanthesha@gmail.com>
2026-01-05 20:58:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useState } from "react";
+
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const reset = () => {
-    setCount(0);
-  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
@@ -30,23 +18,23 @@ function App() {
 
         <div className="flex justify-between mb-4">
           <button
-            onClick={increment}
-            className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
+            onClick={() => setCount(count + 1)}
+            className="bg-green-500 text-white px-4 py-2 rounded-lg"
           >
             Increment
           </button>
 
           <button
-            onClick={decrement}
-            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
+            onClick={() => setCount(count - 1)}
+            className="bg-red-500 text-white px-4 py-2 rounded-lg"
           >
             Decrement
           </button>
         </div>
 
         <button
-          onClick={reset}
-          className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
+          onClick={() => setCount(0)}
+          className="bg-blue-500 text-white px-6 py-2 rounded-lg"
         >
           Reset
         </button>
